15e1930e6fdf136297bbd913c1101870b50bafb2
Putranegara <115201276+PRiauwindu@users.noreply.github.com>
2023-09-01 12:10:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ print(f"Test R2 score: {test_score:.2f}")
 joblib.dump(model, 'california_house_price_model.pkl')
 
 app = Flask(__name__)
-server = app.server
 
 # Load the trained model
 model = joblib.load('california_house_price_model.pkl')
